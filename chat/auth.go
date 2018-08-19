2bb7a4e90cf6d851e1722c3de0927d1f0cf0b52e
Luu Quang Minh <minhmeo753@gmail.com>
2018-08-20 00:59:03 +0700

    Add google oauth

--- chat/auth.go
+++ chat/auth.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/k0kubun/pp"
+
+	"github.com/markbates/goth/gothic"
 )
 
 type authHandler struct {
@@ -24,6 +30,37 @@ func (a *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	// success, call next handler
 	a.next.ServeHTTP(w, r)
+}
+
+// loginHandler handles the third-party login process.
+// format: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	query := r.URL.Query()
+	query.Set("provider", provider)
+	r.URL.RawQuery = query.Encode()
+	switch action {
+	case "login":
+		// try to get the user without re-authenticating
+		if gothUser, err := gothic.CompleteUserAuth(w, r); err == nil {
+			// do some thing after login
+			pp.Println(gothUser.AvatarURL)
+		} else {
+			gothic.BeginAuthHandler(w, r)
+		}
+	case "callback":
+		user, err := gothic.CompleteUserAuth(w, r)
+		if err != nil {
+			fmt.Fprintln(w, err)
+			return
+		}
+		pp.Println(user.AvatarURL)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
+	}
 }
 
 // MustAuth is a Auth hanlder wrapper
--- chat/main.go
+++ chat/main.go
@@ -4,9 +4,13 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/gplus"
 )
 
 type templateHandler struct {
@@ -22,12 +26,27 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.template.Execute(w, r)
 }
 
+var (
+	googleClientID    = os.Getenv("GOOGLE_CLIENT_ID")
+	googleSecret      = os.Getenv("GOOGLE_SECRET")
+	googleRedirectURL = os.Getenv("GOOGLE_REDIRECT_URL")
+)
+
 func main() {
+
 	log.Println("Start server")
 	var addr = flag.String("addr", ":8080", "address of application")
 	flag.Parse()
+
+	// oauth setup
+	goth.UseProviders(
+		gplus.New(googleClientID, googleSecret, googleRedirectURL),
+	)
+
 	r := newRoom()
 	http.Handle("/chat", MustAuth(&templateHandler{fileName: "chat.html"}))
+	http.Handle("/login", &templateHandler{fileName: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// get the room going
 	go r.run()
